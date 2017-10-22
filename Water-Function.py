420d88a6d09d99631e221ee0eb3720b545b6b56e
Ilyamander los Ilyatos <mrend@icloud.com>
2017-10-22 18:39:15 +0300

    ---Version 1.3---  -- Awesome:D -- +GUI for comfortable work (PyQt5) +No crashes while entering a text +Logical structure inside Water (class) +Full code reformation -Unnecessary lines -Redefine function -Some things :)

--- Water-Function.py
+++ Water-Function.py
@@ -1,17 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QLineEdit, QLabel
 
 class Water:
 
-    def __init__(self):
-        self.__f = open("text.txt", "w")
-        try:
-            self.__min_x = float(input("Enter min value of X: "))
-            self.__max_x = float(input("Enter max value of X: "))
-            self.__interval = float(input("Enter interval: "))
-            self.__iteration = float(input("Enter required number of iterations: "))
-            self.__medium_point = self.__min_x + (self.__max_x - self.__min_x) / 2
-        except ValueError:
-            print("You haven't entered the value or smth else :)\n")
-            self.__init__()
+    def set_min(self, min):
+        self.__min_x = float(min)
+
+    def set_max(self, max):
+        self.__max_x = float(max)
+
+    def set_interval(self, interval):
+        self.__interval = float(interval)
+
+    def set_iterations(self, iter):
+        self.__iteration = float(iter)
+        self.__medium_point = self.__min_x + (self.__max_x - self.__min_x) / 2
 
     def __piecewise_function(self, x_value):
         if self.__min_x <= x_value < self.__medium_point:
@@ -28,6 +31,7 @@ class Water:
         return values
 
     def water_spreading(self):
+        self.__f = open("text.txt", "w")
         z_array = self.get_initial_array()
         while self.__iteration > 0:
             for i in range(1, len(z_array)-1):
@@ -41,7 +45,92 @@ class Water:
         self.__f.close()
         return z_array
 
+class Example(QMainWindow):
 
-if __name__ == "__main__":
-    do_it = Water()
-    do_it.water_spreading()
+    def __init__(self):
+        super().__init__()
+        self.w = Water()
+        self.initUI()
+
+
+    def initUI(self):
+
+        runbtn = QPushButton("Run!", self)
+        runbtn.move(90, 250)
+        runbtn.clicked.connect(self.runButtonClicked)
+
+        self.initQLables()
+        self.initQLines()
+
+        self.setGeometry(350, 350, 290, 350)
+        self.setWindowTitle('Spreading')
+        self.show()
+
+    def initQLables(self):
+        minlbl = QLabel("Min x:", self)
+        minlbl.move(50, 25)
+
+        maxlbl = QLabel("Max x:", self)
+        maxlbl.move(50, 75)
+
+        intlbl = QLabel("Interval:", self)
+        intlbl.move(50, 125)
+
+        itrlbl = QLabel("Number of iter.:", self)
+        itrlbl.move(50, 175)
+        #itrlbl.adjustSize()
+
+        self.compl = QLabel("Waiting :)", self)
+        self.compl.move(110, 300)
+
+    def initQLines(self):
+        min_value = QLineEdit(self)
+        min_value.move(50, 50)
+        min_value.textChanged[str].connect(self.minChanged)
+
+        max_value = QLineEdit(self)
+        max_value.move(50, 100)
+        max_value.textChanged[str].connect(self.maxChanged)
+
+        interval = QLineEdit(self)
+        interval.move(50, 150)
+        interval.textChanged[str].connect(self.intervalChanged)
+
+        interations = QLineEdit(self)
+        interations.move(50, 200)
+        interations.textChanged[str].connect(self.iterationsChanged)
+
+    def minChanged(self, text):
+        try:
+            self.w.set_min(text)
+        except:
+            return
+
+    def maxChanged(self, text):
+        try:
+            self.w.set_max(text)
+        except:
+            return
+
+    def intervalChanged(self, text):
+        try:
+            self.w.set_interval(text)
+        except:
+            return
+
+    def iterationsChanged(self, text):
+        try:
+            self.w.set_iterations(text)
+        except:
+            return
+
+    def runButtonClicked(self):
+        self.compl.setText("Computing")
+
+        self.w.water_spreading()
+        self.compl.setText("Done!")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
